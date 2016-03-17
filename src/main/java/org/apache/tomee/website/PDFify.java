e986d7b1404243ec096891b6abe1a7fc9e96376a
Romain manni-Bucau <rmannibucau@gmail.com>
2016-03-17 09:16:34 +0100

    adding download as pdf option

--- src/main/java/org/apache/tomee/website/PDFify.java
+++ src/main/java/org/apache/tomee/website/PDFify.java
@@ -16,12 +16,58 @@
  */
 package org.apache.tomee.website;
 
+import org.asciidoctor.Asciidoctor;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+import static org.asciidoctor.OptionsBuilder.options;
+
 public class PDFify {
     private PDFify() {
         // no-op
     }
 
-    public static void main(final String[] args) {
-        // TODO: read adoc, generate PDF, link it to each page as a download link (adding a template in templates will make it easy)
+    public static void main(final String[] args) throws IOException {
+        final Path sourceBase = Paths.get(args[0]);
+        final File targetBase = new File(args[1]);
+        final Asciidoctor asciidoctor = Asciidoctor.Factory.create();
+        final ExecutorService pool = Executors.newFixedThreadPool(16);
+        Files.walkFileTree(sourceBase, new SimpleFileVisitor<Path>() {
+            @Override
+            public FileVisitResult visitFile(final Path file, final BasicFileAttributes attrs) throws IOException {
+                final String fileName = file.getFileName().toString();
+                if (fileName.endsWith(".adoc")) {
+                    pool.submit(() -> {
+                        final String path = sourceBase.relativize(file).toString();
+                        final File target = new File(targetBase, path.substring(0, path.length() - "adoc".length()) + "pdf");
+                        final File asFile = file.toFile();
+                        if (asciidoctor.readDocumentHeader(asFile).getAttributes().containsKey("jbake-tomeepdf")) {
+                            asciidoctor.convertFile(
+                                    asFile,
+                                    options().backend("pdf").toFile(target).get());
+                            System.out.println("Generated " + target);
+                        }
+                    });
+                }
+                // TODO: read adoc, generate PDF, link it to each page as a download link (adding a template in templates will make it easy
+                return super.visitFile(file, attrs);
+            }
+        });
+        pool.shutdown();
+        try {
+            pool.awaitTermination(1, TimeUnit.HOURS);
+        } catch (final InterruptedException e) {
+            Thread.interrupted();
+        }
     }
 }
--- src/main/jbake/templates/page.gsp
+++ src/main/jbake/templates/page.gsp
@@ -6,6 +6,11 @@
             <h2>${content.title}</h2>
         </div>
         <div class="row">
+            <%if (content.containsKey('tomeepdf')) {%>
+            <div class="col-md-12">
+                <i class="fa fa-file-pdf-o"></i> <a href="<%if (content.rootpath) {%>${content.rootpath}<% } else { %><% }%>${content.uri.replace('html', 'pdf')}">Download as PDF</a>
+            </div>
+            <% } %>
             <div class="col-md-12">
                 ${content.body}
             </div>
